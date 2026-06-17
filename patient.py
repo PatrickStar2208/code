c215c198ef799770546016f4ac76eb23e9fe7cf3
kaihuynh06092007 <kaihuynh06092007@gmail.com>
2026-06-17 10:41:20 +0700

    Update patient.py

--- patient.py
+++ patient.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+
 class patient:
     def __init__(self, name, ID, sympton):
         self.name = name
         self.ID = ID
         self.sympton = sympton
+        self.registration_time = datetime.now()
     def __str__(self):
-        return f"{self.name} - {self.ID} - sympton:{self.sympton}"
+        return f"{self.name} - {self.ID} - sympton:{self.sympton} - Registered: {self.registration_time.strftime( '%d/%m/%Y %H:%M:%S' )}"
